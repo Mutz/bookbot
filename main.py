4c65e2b4ba38631180a1379687445ee8d9adb029
Martin Schilling <mutz@mutzs.org>
2025-08-08 00:47:06 +0200

    completed get_book_text function and updated .gitignore

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-def get_book_text("book"):
-    
+def get_book_text(path_to_book):
+    with open(path_to_book, 'r') as f:
+        book_content_text = f.read()
+        return book_content_text
+
+if __name__ == '__main__':
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
