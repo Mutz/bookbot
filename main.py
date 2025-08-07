012092c34fc1e6ae6a647510fb987d0806b10636
Martin Schilling <mutz@mutzs.org>
2025-08-08 01:05:23 +0200

    added get_word_count function and updated main script to display word count

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ def get_book_text(path_to_book):
         book_content_text = f.read()
         return book_content_text
 
+def get_word_count(text_string):
+    word_list = text_string.split()
+    return len(word_list)
+
 if __name__ == '__main__':
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    print(f"{get_word_count(book_text)} words found in the document")
