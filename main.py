aa8b77298868af6dcb7a5f8a7f78c127e2cc6a54
Mutz <mutz@mutzs.org>
2025-08-08 01:39:13 +0200

    Extract `get_word_count` function to a separate `stats.py` module.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import get_word_count
+
 def get_book_text(path_to_book):
     with open(path_to_book, 'r') as f:
         book_content_text = f.read()
         return book_content_text
-
-def get_word_count(text_string):
-    word_list = text_string.split()
-    return len(word_list)
 
 if __name__ == '__main__':
     book_text = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_word_count(text_string):
+    word_list = text_string.split()
+    return len(word_list)
